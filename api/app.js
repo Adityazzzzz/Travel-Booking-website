be868f99a82a0d39bf6fd5fefe7a82f41b190ca5
Aditya Singh <160380902+Adityazzzzz@users.noreply.github.com>
2025-04-16 14:00:10 +0530

    port fix

--- api/app.js
+++ api/app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(user)
 
 
-const port= 5000 || process.env.MONGO_URL;
+const port=process.env.PORT || 5000;
 const start=async()=>{
     try {
         await connectDB(process.env.MONGO_URL)
