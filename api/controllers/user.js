e19669a24488412969c4cd06d5b4515866edee1d
Aditya Singh <160380902+Adityazzzzz@users.noreply.github.com>
2025-04-16 15:50:23 +0530

    fix 4

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,8 +8,8 @@ const imagedownloader = require('image-downloader')
 
 function getUserDatafromtoken(req){
     return new Promise ((resolve,reject)=>{
-        jwt.verify(req.cookies.token, process.env.JWT_SECRET, {}, async (err, userData) => {
-            if(err) throw err;
+        jwt.verify(req.cookies?.token, process.env.JWT_SECRET, {}, async (err, userData) => {
+            if(err) reject(err);
             resolve(userData)
         })
     })
